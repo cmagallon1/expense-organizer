418ddd3d918414730f35f0cdc51145a0664815b9
cesar magallon <cesar9mv@gmail.com>
2019-05-07 23:45:15 -0500

    [add]webpack

--- src/index.js
+++ src/index.js
@@ -6,6 +6,40 @@ const dbx = new Dropbox({
     fetch
 });
 
-dbx.filesListFolder({
-    path: ''//this specify the root path
-}).then(res => console.log(res))
+const fileListElem = document.querySelector('.js-file-list');
+
+const state = {
+    files: []
+}
+
+const init = () => {
+    dbx.filesListFolder({
+        path: ''//this specify the root path
+    }).then(res => {
+        updatedFiles(res.entries)
+    })    
+}
+
+const updatedFiles = files => {
+    state.files = [...state.files, ...files]
+    renderFiles()
+}
+
+const renderFiles = () => {
+    fileListElem.innerHTML = state.files.sort((a,b) => {
+        //sort alphabetically, folders fisrt
+        if((a['.tag'] === 'folder' || b['.tag'] === 'folder')
+            && !(a['.tag'] === b['.tag'])){
+                return a['.tag'] === 'folder' ? -1 : 1
+        } else {
+            return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+        }
+    }).map(file => {
+        const type = file['.tag']
+        return `
+            <li class="dbx-list-item ${type}">${file.name}</li>
+        `
+    })
+}
+
+init();
